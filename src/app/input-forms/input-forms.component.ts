26e97f5429f4fb1053f5df1af1fe37a8bd0c75b0
Shoaib Ghalib <shoaib.ghalib@hotmail.com>
2019-02-06 00:59:44 +0500

    input forms page completed

--- src/app/input-forms/input-forms.component.ts
+++ src/app/input-forms/input-forms.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 
+export class MyItems {
+  Value: string;
+  constructor(Value: string) {
+    this.Value = Value;
+  }
+}
+
 @Component({
   selector: 'app-input-forms',
   templateUrl: './input-forms.component.html',
@@ -9,62 +16,107 @@ import { FormControl, FormGroup } from '@angular/forms';
 export class InputFormsComponent implements OnInit {
 
   fajr;
-  zuhr;
+  dohr;
   asar;
-  maghrb;
+  maghrib;
   isha;
   jumma;
-  announcements;
-  news;
 
+  namazTimings = {}
+
+
+  // Array where we are going to do CRUD operations    
+  myItems: MyItems[] = new Array();
+
+  // Other variables    
+  IsForUpdate: boolean = false;
+  newItem: any = {};
+  updatedItem;
 
   constructor() {
-    this.fajr = localStorage.getItem("fajr");
-    this.zuhr = localStorage.getItem("zuhr");
-    this.asar = localStorage.getItem("asar");
-    this.maghrb = localStorage.getItem("maghrb");
-    this.isha = localStorage.getItem("isha");
-    this.jumma = localStorage.getItem("jumma");
-    this.announcements = localStorage.getItem("announcements")
-    this.news = localStorage.getItem("news")
+    this.namazTimings = JSON.parse(localStorage.getItem("namazTimings"));
+    if(this.namazTimings){
+      this.fajr = this.namazTimings['fajar'];
+      this.dohr = this.namazTimings['dohr'];
+      this.asar = this.namazTimings['asar'];
+      this.maghrib = this.namazTimings['maghrib'];
+      this.isha = this.namazTimings['isha'];
+      this.jumma = this.namazTimings['jumma'];
+    }
 
-   }
+    this.myItems = JSON.parse(localStorage.getItem("announcements"));
+    if(!this.myItems){
+      this.myItems = [];
+    }
 
-  
+    
+  }
+
+
 
   ngOnInit() {
+   
   }
 
-  submit(){
-    localStorage.setItem("fajr", this.fajr);
-    localStorage.setItem("zuhr", this.zuhr);
-    localStorage.setItem("asar", this.asar);
-    localStorage.setItem("maghrb", this.maghrb);
-    localStorage.setItem("isha", this.isha);
-    localStorage.setItem("jumma", this.jumma);
-    localStorage.setItem("announcements", this.announcements);
-    localStorage.setItem("news", this.news);
-  
-    console.log(this.formatAMPM(this.fajr))
-    console.log(this.formatAMPM(this.zuhr))
-    console.log(this.formatAMPM(this.asar))
-    console.log(this.formatAMPM(this.maghrb))
-    console.log(this.formatAMPM(this.isha))
-    console.log(this.formatAMPM(this.jumma)) 
-    console.log(this.announcements, 'ann')
-    console.log(this.news, 'n')
+  submit() {
+    this.namazTimings = {
+      fajar: this.fajr,
+      dohr: this.dohr,
+      asar: this.asar,
+      maghrib: this.maghrib,
+      isha: this.isha,
+      jumma: this.jumma
+    }
 
+    localStorage.setItem("namazTimings", JSON.stringify(this.namazTimings));
+    localStorage.setItem("announcements", JSON.stringify(this.myItems));
   }
 
-  formatAMPM(date) {
-    var hours = date.getHours();
-    var minutes = date.getMinutes();
-    var ampm = hours >= 12 ? 'pm' : 'am';
-    hours = hours % 12;
-    hours = hours ? hours : 12; // the hour '0' should be '12'
-    minutes = minutes < 10 ? '0'+minutes : minutes;
-    var strTime = hours + ':' + minutes + ' ' + ampm;
-    return strTime;
+  // formatAMPM(date) {
+  //   var hours = date.getHours();
+  //   var minutes = date.getMinutes();
+  //   var ampm = hours >= 12 ? 'pm' : 'am';
+  //   hours = hours % 12;
+  //   hours = hours ? hours : 12; // the hour '0' should be '12'
+  //   minutes = minutes < 10 ? '0' + minutes : minutes;
+  //   var strTime = hours + ':' + minutes + ' ' + ampm;
+  //   return strTime;
+  // }
+
+  // To add new item in array    
+  AddItem() {
+    this.myItems.push(
+      this.newItem
+    );
+    this.newItem = {};
+    localStorage.setItem("announcements", JSON.stringify(this.myItems));
+  }
+
+  // When user selects edit option  
+  EditItem(i) {
+    this.newItem.Value = this.myItems[i].Value;
+    this.updatedItem = i;
+    this.IsForUpdate = true;
+  }
+
+  // When user clicks on update button to submit updated value  
+  UpdateItem() {
+    let data = this.updatedItem;
+    for (let i = 0; i < this.myItems.length; i++) {
+      if (i == data) {
+        this.myItems[i].Value = this.newItem.Value;
+        localStorage.setItem("announcements", JSON.stringify(this.myItems));
+
+      }
+    }
+    this.IsForUpdate = false;
+    this.newItem = {};
+  }
+
+  // To delete specific item  
+  DeleteItem(i) {
+    this.myItems.splice(i, 1);
+    localStorage.setItem("announcements", JSON.stringify(this.myItems));
   }
 
 }
